43c3efb702eca8c3988f51f41c2709758cbb5290
mmrxsky <mmrxsky@gmail.com>
2024-10-01 22:42:15 +0300

    Changes pep8

--- src/database.py
+++ src/database.py
@@ -2,16 +2,16 @@ import psycopg2
 
 
 def create_database(params):
-    '''
+    """
     Создаем базу данных coursework_5
-    '''
-    conn = psycopg2.connect(dbname='cw5',**params)
-# conn = psycopg2.connect(
-#     host="localhost",
-#     user="postgres",
-#     password="2468",
-#     port=5432
-# )
+    """
+    conn = psycopg2.connect(dbname="cw5", **params)
+    # conn = psycopg2.connect(
+    #     host="localhost",
+    #     user="postgres",
+    #     password="2468",
+    #     port=5432
+    # )
     conn.autocommit = True
     cur = conn.cursor()
     cur.execute("""DROP DATABASE IF EXISTS courswork5""")
